66b78fec1c285872335c8786aa79f70007e50e33
Helmisofia <helmi.huttunen@solidabis.com>
2024-04-02 12:14:18 +0300

    varmuuskopiointi

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -3,7 +3,7 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
-import '../App.css'
+import '../App.css';
 import moment from 'moment';
 
 export default function Calendar() {
